b0413155da7fb53af33f4ddc4cd1c4908764f9d2
sofiatoroviafara01 <sofiaviafara123@gmail.com>
2025-09-30 17:32:33 +0200

    file organization

---
+++ src/main/java/com/biblioteca_feminista/App.java
@@ -0,0 +1,13 @@
+package com.biblioteca_feminista;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
